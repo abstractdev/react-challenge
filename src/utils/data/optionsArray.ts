c6ef43ff0e78e83b577d3ca5d8e6f4a4c29f278b
Andrew R <abstractdev0@gmail.com>
2022-10-08 14:48:07 -0400

    feat: create optionsArray

---
+++ src/utils/data/optionsArray.ts
@@ -0,0 +1,9 @@
+const optionsArray = [
+  { value: "", text: "All Ratings" },
+  { value: "5", text: "5 Stars" },
+  { value: "4", text: "4 Stars" },
+  { value: "3", text: "3 Stars" },
+  { value: "2", text: "2 Stars" },
+  { value: "1", text: "1 Stars" },
+];
+export default optionsArray;
